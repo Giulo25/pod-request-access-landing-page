2e967012aac380d75232f7cd7a4b740d332f1918
Simone <82872761+Giulo25@users.noreply.github.com>
2022-11-20 15:17:21 +0100

    Versione Desktop
    Versione desktop con javascript ok

---
+++ script.js
@@ -0,0 +1,33 @@
+const submit = document.querySelector("#button");
+const email = document.querySelector("#email");
+const body = document.querySelector("body");
+const formContainer = document.querySelector(".form-container");
+const formMessage = document.querySelector(".form-message");
+
+/* validate email */
+const validateEmail = function (mail) {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+		return true;
+	}
+	return false;
+};
+
+const validEmail = function () {
+	// body.classList.remove("error");
+	formMessage.innerHTML = "Thanks for your subscription!";
+	formMessage.style.color = "#54E6AF";
+	email.setAttribute("placeholder", "Email address");
+	email.value = "";
+};
+const invalidEmail = function () {
+	body.classList.add("error");
+};
+
+submit.addEventListener("click", function (e) {
+	if (validateEmail()) {
+		validEmail();
+		this.disabled = true;
+	} else {
+		invalidEmail();
+	}
+});
